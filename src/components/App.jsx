771385a322bdfcf1827297534748773dffb498b7
Philip Machar <philipmachar788@gmail.com>
2023-07-21 11:29:06 +0300

    fourth commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import { CssBaseline } from '@mui/material';
-import { Routes, Route, } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import { RootContainer, Toolbar, Content } from './styles';
 
 import { Actors, MovieInformation, Movies, Navbar, Profile } from './index';
 
 const App = () => {
   return (
-    <div>
+    <RootContainer>
       <CssBaseline />
       <Navbar />
-      <main>
+      <Content>
+        <Toolbar />
         <Routes>
           <Route exact path="/" element={<Movies />} />
           <Route path="/actors/:id" element={<Actors />} />
           <Route path="/movie/:id" element={<MovieInformation />} />
           <Route path="/profile/:id" element={<Profile />} />
         </Routes>
-      </main>
-    </div>
+      </Content>
+    </RootContainer>
   );
 };
 
